6e6518af68a57a838c2b9088fd158c614942300e
Dmitry Kotov <xeno93@gmail.com>
2019-08-12 11:55:46 +0300

    Rearrange folders

---
+++ Tutorials/Roll-A-Ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private GameObject player = default;
+
+    private Vector3 _offset;
+
+    private void Start()
+    {
+        _offset = transform.position - player.transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = player.transform.position + _offset;
+    }
+}
---
+++ Tutorials/Roll-A-Ball/Assets/Scripts/PickUpRotator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpRotator : MonoBehaviour
+{
+    private const float RotationSpeed = 135F;
+
+    private void Update()
+    {
+        transform.rotation *= Quaternion.AngleAxis(RotationSpeed * Time.deltaTime, new Vector3(0, 1));
+    }
+}
