306aefa1340d502b372bba9e26440e2dfab223cd
Dmitry Kotov <xeno93@gmail.com>
2019-08-25 18:15:59 +0300

    3D Beginner add ghosts

---
+++ Tutorials/3D Beginner/Assets/Scripts/WaypointPatrol.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class WaypointPatrol : MonoBehaviour
+{
+    private int _currentWaybillIndex = 0;
+    [SerializeField] private Transform[] waybill;
+    [SerializeField] private NavMeshAgent navMeshAgent;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        navMeshAgent.SetDestination(waybill[0].position);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (!(navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance)) return;
+        
+        _currentWaybillIndex = (_currentWaybillIndex + 1) % waybill.Length;
+        navMeshAgent.SetDestination (waybill[_currentWaybillIndex].position);
+    }
+}
