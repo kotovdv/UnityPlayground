49b4db64876494943046640a04853a3b38d0f62a
Dmitry Kotov <xeno93@gmail.com>
2019-08-25 16:19:38 +0300

    3D Beginner add gargoyle

--- Tutorials/3D Beginner/Assets/Scripts/GameEnding.cs
+++ Tutorials/3D Beginner/Assets/Scripts/GameEnding.cs
@@ -1,15 +1,18 @@
 ﻿using System;
 using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameEnding : MonoBehaviour
 {
     private const float FadeDuration = 1f;
     private const float DisplayEndGameImageDuration = 2f;
-    
-    [SerializeField] private GameObject player;
-    [SerializeField] private CanvasGroup gameEndingCanvasGroup;
 
+    [SerializeField] private GameObject player;
+    [SerializeField] private CanvasGroup positiveEndingCanvasGroup;
+    [SerializeField] private CanvasGroup negativeEndingCanvasGroup;
+
+    public bool WasCaught { private get; set; }
     private bool _hasTriggeredExitSequence;
     private readonly Stopwatch _stopwatch = new Stopwatch();
 
@@ -17,17 +20,35 @@ public class GameEnding : MonoBehaviour
     {
         if (other.gameObject != player) return;
         _hasTriggeredExitSequence = true;
-        _stopwatch.Start();
+
     }
 
     private void Update()
     {
-        if (!_hasTriggeredExitSequence) return;
+        if (_hasTriggeredExitSequence)
+        {
+            EndLevel(positiveEndingCanvasGroup, false);
+        }
+        else if (WasCaught)
+        {
+            EndLevel(negativeEndingCanvasGroup, true);
+        }
+    }
+
+    private void EndLevel(CanvasGroup canvasGroup, bool restartLevel)
+    {
+        _stopwatch.Start();
         
         var elapsedSeconds = _stopwatch.Elapsed.TotalSeconds;
-        gameEndingCanvasGroup.alpha = (float) (elapsedSeconds / FadeDuration);
+        canvasGroup.alpha = (float) (elapsedSeconds / FadeDuration);
+
+        if (!(elapsedSeconds > FadeDuration + DisplayEndGameImageDuration)) return;
         
-        if (elapsedSeconds > FadeDuration + DisplayEndGameImageDuration)
+        if (restartLevel)
+        {
+            SceneManager.LoadScene(0);
+        }
+        else
         {
             Application.Quit();
         }
---
+++ Tutorials/3D Beginner/Assets/Scripts/Observer.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class Observer : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private GameEnding gameEnding;
+
+    private bool _canSeePlayer;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (!_canSeePlayer) return;
+        
+        var observerPosition = transform.position;
+        var direction = (player.position + Vector3.up) - observerPosition;
+        var ray = new Ray(observerPosition, direction);
+
+        if (!Physics.Raycast(ray, out var raycastHit)) return;
+            
+        if (raycastHit.collider.transform == player)
+        {
+            gameEnding.WasCaught = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform != player) return;
+        _canSeePlayer = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform != player) return;
+        _canSeePlayer = false;
+    }
+}
