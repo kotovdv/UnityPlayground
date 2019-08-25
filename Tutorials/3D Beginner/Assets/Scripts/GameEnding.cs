e3902b31fbc211495c900e51c5f0c57d0e231f7f
Dmitry Kotov <xeno93@gmail.com>
2019-08-25 11:59:02 +0300

    3D Beginner add end game logic

---
+++ Tutorials/3D Beginner/Assets/Scripts/GameEnding.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using UnityEngine;
+
+public class GameEnding : MonoBehaviour
+{
+    private const float FadeDuration = 1f;
+    private const float DisplayEndGameImageDuration = 2f;
+    
+    [SerializeField] private GameObject player;
+    [SerializeField] private CanvasGroup gameEndingCanvasGroup;
+
+    private bool _hasTriggeredExitSequence;
+    private readonly Stopwatch _stopwatch = new Stopwatch();
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject != player) return;
+        _hasTriggeredExitSequence = true;
+        _stopwatch.Start();
+    }
+
+    private void Update()
+    {
+        if (!_hasTriggeredExitSequence) return;
+        
+        var elapsedSeconds = _stopwatch.Elapsed.TotalSeconds;
+        gameEndingCanvasGroup.alpha = (float) (elapsedSeconds / FadeDuration);
+        
+        if (elapsedSeconds > FadeDuration + DisplayEndGameImageDuration)
+        {
+            Application.Quit();
+        }
+    }
+}
